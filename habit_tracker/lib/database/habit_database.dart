912e74d4f76e0fa94252778370d75edb19cdf75b
Hanna Yohannes <hanyohmek@gmail.com>
2025-02-21 14:33:49 +0300

    habit_database.dart updated

--- habit_tracker/lib/database/habit_database.dart
+++ habit_tracker/lib/database/habit_database.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/animation.dart';
 import 'package:flutter/foundation.dart';
+import 'package:habit_tracker/models/app_settings.dart';
+import 'package:habit_tracker/models/habit.dart';
 import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
 
 class HabitDatabase extends ChangeNotifier {
   static late Isar isar;
   //initialize database
+  static Future<void> initialize() async {
+    final dir = await getApplicationDocumentsDirectory();
+    isar = await Isar.open(
+      [HabitSchema, AppSettingsSchema],
+      directory: dir.path,
+    );
+  }
 }
