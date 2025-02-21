31461ffdd5083f6afbaabd8a5b844929f7342d0d
Hanna Yohannes <hannayohmek@gmail.com>
2025-02-21 14:16:04 +0300

    database class update

--- habit_tracker/lib/database/habit_database.dart
+++ habit_tracker/lib/database/habit_database.dart
@@ -1,2 +1,7 @@
 import 'package:flutter/animation.dart';
+import 'package:flutter/foundation.dart';
 import 'package:isar/isar.dart';
+
+class HabitDatabase extends ChangeNotifier {
+  static late Isar isar;
+}
